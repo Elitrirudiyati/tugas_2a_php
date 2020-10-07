4c3fe0fc47318629a95790ab66e2cd20985ee53b
Elitrirudiyati <71365611+Elitrirudiyati@users.noreply.github.com>
2020-10-07 16:33:07 +0700

    Add files via upload

---
+++ tugas_2a.php
@@ -0,0 +1,11 @@
+<?php
+$hari1 = 36;
+$pekerja1 = 12;
+$pekerja2 = 24;
+$hari2 = ($pekerja1 / $pekerja2) * $hari1;
+
+print_r ($hari1." Hari = ".$pekerja1." Pekerja <br>");
+print_r ("X Hari = ".$pekerja2." Pekerja <br>");
+print_r("X = ...? <br>");
+print_r("Jawaban = ".$hari2);
+?>
